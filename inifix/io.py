a8652f103f89d636131af855cc8fa5a8b79afb1f
Clément Robert <cr52@protonmail.com>
2021-03-13 17:58:56 +0100

    more tests for api

--- inifix/io.py
+++ inifix/io.py
@@ -5,10 +5,10 @@ def load(dict_or_path_or_buffer, /) -> IdefixConf:
     return IdefixConf(dict_or_path_or_buffer)
 
 
-def dump(conf: dict, file_descriptor) -> None:
+def dump(conf: dict, file_descriptor, /) -> None:
     """Write a `conf` dict to a file.
 
-    `file_desciptor` can represent a path or an opened file.
+    `file_descriptor` can represent a file path or an object with a `write` method.
     """
     conf = IdefixConf(conf)
     conf.write(file_descriptor)
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,3 +1,5 @@
+import pytest
+
 from inifix.io import dump, load
 
 
@@ -10,13 +12,44 @@ def test_load(inifile):
         assert all([isinstance(param, str) for param in body])
 
 
-def test_dump_known_files(inifile, tmp_path):
+@pytest.mark.parametrize(
+    "mode,expected_err",
+    [("rt", FileNotFoundError), ("rb", FileNotFoundError), ("wb", TypeError)],
+)
+def test_dump_wrong_mode(mode, expected_err, inifile, tmp_path):
+    conf = load(inifile)
+    save_file = str(tmp_path / "save.ini")
+    with pytest.raises(expected_err):
+        with open(save_file, mode=mode) as fh:
+            dump(conf, fh)
+
+
+def test_dump_to_file_descriptor(inifile, tmp_path):
     conf = load(inifile)
 
     file = tmp_path / "save.ini"
-    dump(conf, file)
+    with open(file, mode="w") as fh:
+        dump(conf, fh)
 
     # a little weak but better than just testing that the file isn't empty
     new_body = file.read_text()
     for section_name in conf:
         assert f"[{section_name}]\n" in new_body
+
+
+def test_dump_to_file_path(inifile, tmp_path):
+    conf = load(inifile)
+
+    # pathlib.Path obj
+    file1 = tmp_path / "save1.ini"
+    dump(conf, file1)
+    body1 = file1.read_text()
+
+    # str
+    file2 = str(tmp_path / "save2.ini")
+    dump(conf, file2)
+    body2 = open(file2).read()
+
+    assert body1 == body2
+    for section_name in conf:
+        assert f"[{section_name}]\n" in body1
