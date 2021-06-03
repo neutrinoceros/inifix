83100d02be5dfd6630825a0e79f1bd6dbbd48027
Clément Robert <cr52@protonmail.com>
2021-06-03 15:03:31 +0200

    BUG: fix schema validation (restore compat with fargo planet-files)

--- inifix/validation.py
+++ inifix/validation.py
@@ -1,4 +1,24 @@
-from typing import Sequence
+from more_itertools import always_iterable
+
+
+def validate_elementary_item(key, value, /) -> None:
+    scalar_types = (int, float, bool, str)
+    if not isinstance(key, str):
+        raise ValueError(
+            f"Invalid schema: received key '{key}' with type '{type(key)}', "
+            "expected a str"
+        )
+    if not isinstance(value, (*scalar_types, list)):
+        raise ValueError(
+            f"Invalid schema: received value with type '{type(value)}', "
+            "exepected an int, float, bool, str, or list"
+        )
+    for ev in always_iterable(value):
+        if not isinstance(ev, scalar_types):
+            raise ValueError(
+                f"Invalid schema: reveived value '{value}' with type '{type(value)}', "
+                "exepected a int, float, bool or str"
+            )
 
 
 def validate_inifile_schema(d: dict, /) -> None:
@@ -6,20 +26,14 @@ def validate_inifile_schema(d: dict, /) -> None:
     Raise `ValueError` if and only if the argument is not a
     valid configuration according to `inifix.InifixConf` specifications.
     """
-    scalar_types = (int, float, bool, str)
-    err = ValueError("Invalid schema detected.")
+
     for k, v in d.items():
         if not isinstance(k, str):
-            raise err
+            raise ValueError(
+                f"Invalid schema: received key '{k}' with type '{type(k)}', expected a str"
+            )
         if isinstance(v, dict):
             for kk, vv in v.items():
-                if not isinstance(kk, str):
-                    raise err
-                if isinstance(vv, Sequence):
-                    for vvv in vv:
-                        if not isinstance(vvv, scalar_types):
-                            raise err
-                elif not isinstance(vv, scalar_types):
-                    raise err
-        elif not isinstance(v, scalar_types):
-            raise err
+                validate_elementary_item(kk, vv)
+        else:
+            validate_elementary_item(k, v)
