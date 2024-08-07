f6d51e2f9ed44f6d7f4cdb43239e67edc80a643a
Clément Robert <cr52@protonmail.com>
2024-08-07 16:12:09 +0200

    CLN: cleanup unused private code

--- src/inifix/_more.py
+++ src/inifix/_more.py
@@ -4,9 +4,6 @@ from typing import Any
 
 def always_iterable(obj: Any, /) -> Iterator[Any]:
     # adapated from more_iterools 10.1.0 (MIT)
-    if obj is None:
-        return iter(())
-
     if isinstance(obj, str):
         return iter((obj,))
 
