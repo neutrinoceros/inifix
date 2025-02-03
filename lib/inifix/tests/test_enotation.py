297b9fa2830eec06fbdfa5e3435a996e6d66d5a3
Clément Robert <cr52@protonmail.com>
2025-02-03 16:26:13 +0100

    TST: add property-based tests for enotation module

--- lib/inifix/tests/test_enotation.py
+++ lib/inifix/tests/test_enotation.py
@@ -1,4 +1,8 @@
+from math import isnan
+
 import pytest
+from hypothesis import example, given
+from hypothesis import strategies as st
 
 from inifix._enotation import Enotation
 
@@ -52,3 +56,32 @@ def test_always_encode(input, expected):
 )
 def test_only_shorter_encode(input, expected):
     assert Enotation.ONLY_SHORTER.encode(input) == expected
+
+
+# for reliable coverage, include a mandatory example for
+# each branch in the test
+@example(100.0)  # len('1e2') < len('100.0')
+@example(1.0)  # len('1e0') == len('1.0')
+@example(0.5)  # len('5e-1') > len('0.5')
+@example(float("nan"))
+@given(st.floats())
+def test_strategies_comparison(r):
+    s0 = str(r)
+    s1 = Enotation.ALWAYS.encode(r)
+    s2 = Enotation.ONLY_SHORTER.encode(r)
+
+    if isnan(r):
+        assert isnan(float(s1))
+        assert isnan(float(s2))
+    else:
+        assert float(s1) == r
+        assert float(s2) == r
+
+    assert len(s2) <= len(s1)
+    assert len(s2) <= len(s0)
+    assert s2 in (s1, s0)
+
+    if len(s1) < len(s0):
+        assert s2 == s1
+    else:
+        assert s2 == s0
