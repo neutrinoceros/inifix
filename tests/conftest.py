87485cc2a9c7656f7b882a45880aa67ee3dfd1b7
Clément Robert <cr52@protonmail.com>
2025-08-09 16:58:51 +0200

    TST: add support for CPython's experimental JIT compiler

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import sys
+from enum import Enum
 from pathlib import Path
 
 import pytest
@@ -20,12 +21,39 @@ def inifile_root(request):
     return request.param
 
 
-def pytest_report_header(config, start_path):
-    if sys.version_info >= (3, 13):
-        is_gil_enabled = sys._is_gil_enabled()
+class CPythonFeatureStatus(Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+    UNAVAILABLE = "unavailable"
+    UNKNOWN = "unknown"
+
+
+def pytest_report_header(config, start_path) -> list[str]:
+    if sys.implementation.name != "cpython":
+        return []  # pragma: no cover
+
+    jit_st: CPythonFeatureStatus
+    if sys.version_info >= (3, 14):
+        if sys._jit.is_enabled():
+            jit_st = CPythonFeatureStatus.ENABLED
+        else:
+            jit_st = CPythonFeatureStatus.DISABLED
+    elif sys.version_info >= (3, 13):
+        jit_st = CPythonFeatureStatus.UNKNOWN  # no public API for introspection
     else:
-        is_gil_enabled = True
+        jit_st = CPythonFeatureStatus.UNAVAILABLE
+
+    free_threading_st: CPythonFeatureStatus
+    if sys.version_info >= (3, 13):
+        if sys._is_gil_enabled():
+            free_threading_st = CPythonFeatureStatus.DISABLED
+        else:
+            free_threading_st = CPythonFeatureStatus.ENABLED
+    else:
+        free_threading_st = CPythonFeatureStatus.UNAVAILABLE
 
     return [
-        f"{is_gil_enabled = }",
+        "CPython optional features:",
+        f"  free-threading: {free_threading_st.value}",
+        f"  JIT: {jit_st.value}",
     ]
